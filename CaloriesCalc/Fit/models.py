6ce0b31393a9d035c1ade1197283866ea75a99dd
Architect <greatraksin@icloud.com>
2020-11-29 17:02:21 +0300

    Adding Category model at models.py

--- CaloriesCalc/Fit/models.py
+++ CaloriesCalc/Fit/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
@@ -14,4 +14,13 @@ class Customer(models.Model):
 
 
 class Category(models.Model):
-    pass
+    options = (
+        ('breakfast', 'Завтрак'),
+        ('lunch', 'Обед'),
+        ('dinner', 'Ужин'),
+        ('snack', 'Перекус')
+    )
+    name = models.CharField(max_length=50, choices=options)
+
+    def __str__(self):
+        return str(self.name)
