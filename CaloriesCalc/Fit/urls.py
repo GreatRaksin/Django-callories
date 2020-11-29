a6dbdf989d5658c89aa9691e340a10265f4547b0
Architect <greatraksin@icloud.com>
2020-11-29 18:35:21 +0300

    Finishing urls.py file

--- CaloriesCalc/Fit/urls.py
+++ CaloriesCalc/Fit/urls.py
@@ -2,3 +2,29 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('user/', views.userPage, name='userPage'),
+    path('product/', views.fooditem, name='fooditem'),
+    path('createfooditem/', views.createfooditem, name='createfooditem'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('addFooditem/', views.addFooditem, name='addFooditem'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(),
+         name='reset_password'),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+
+    path('reset/<uid64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+]
