31c5519e6a403d22677516ce74153f9d961cfad6
demidraksin <greatraksin@icloud.com>
2020-11-29 17:18:21 +0300

    Adding FoodItem model at models.py

--- CaloriesCalc/Fit/models.py
+++ CaloriesCalc/Fit/models.py
@@ -24,3 +24,16 @@ class Category(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ManyToManyField(Category)
+    protein = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    fats = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    carbohydrate = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    calorie = models.DecimalField(max_digits=5, decimal_places=2, default=0, blank=True)
+    quantity = models.IntegerField(default=1, null=True, blank=True)
+
+    def __str__(self):
+        return
