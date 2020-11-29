7b809e4a941f56d4da79330ac3ee8ba2b23401af
Architect <greatraksin@icloud.com>
2020-11-29 18:02:29 +0300

    Adding class Foodadmin

--- CaloriesCalc/Fit/admin.py
+++ CaloriesCalc/Fit/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+
+class FoodAdmin(admin.ModelAdmin):
+    class Meta:
+        model = FoodItem
+
+    list_display = ['name']
+    list_filter = ['name']
