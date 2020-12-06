a8d04e58328da70612003df0860f57dc55e8a5a7
demidraksin <greatraksin@icloud.com>
2020-12-06 16:54:35 +0300

    Adding fooditem view

--- CaloriesCalc/Fit/views.py
+++ CaloriesCalc/Fit/views.py
@@ -31,17 +31,24 @@ def home(requet):
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
-def home(requet):
-    breakfast = Category.objects.filter(name='breakfast')[0].fooditem_set.all()[:5]
-    lunch = Category.objects.filter(name='lunch')[0].fooditem_set.all()[:5]
-    dinner = Category.objects.filter(name='dinner')[0].fooditem_set.all()[:5]
-    snack = Category.objects.filter(name='snack')[0].fooditem_set.all()[:5]
-    customers = Customer.objects.all()
+def fooditem(requet):
+    breakfast = Category.objects.filter(name='breakfast')[0].fooditem_set.all()
+    b_count = breakfast.count()
+    lunch = Category.objects.filter(name='lunch')[0].fooditem_set.all()
+    l_count = lunch.count()
+    dinner = Category.objects.filter(name='dinner')[0].fooditem_set.all()
+    d_count = dinner.count()
+    snack = Category.objects.filter(name='snack')[0].fooditem_set.all()
+    s_count = snack.count()
+
     context = {
         'breakfast': breakfast,
+        'b_count': b_count,
         'lunch': lunch,
+        'l_count': l_count,
         'dinner': dinner,
+        'd_count': d_count,
         'snack': snack,
-        'customers': customers
+        's_count': s_count,
     }
-    return render(requet, 'main.html', context)
+    return render(requet, 'fooditem.html', context)
