d5f8c2ccb27025508b0e2b6352bdca45379dd1ee
demidraksin <greatraksin@icloud.com>
2020-12-06 16:41:19 +0300

    Adding imports to views.py

--- CaloriesCalc/Fit/views.py
+++ CaloriesCalc/Fit/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import *
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .decorators import *
+from .filter import *
+
