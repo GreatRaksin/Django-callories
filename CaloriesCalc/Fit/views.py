a1a0ebe75baee7fc6e73116bd91a479d68b28ed2
demidraksin <greatraksin@icloud.com>
2020-12-06 16:50:21 +0300

    Adding home view

--- CaloriesCalc/Fit/views.py
+++ CaloriesCalc/Fit/views.py
@@ -10,3 +10,38 @@ from django.contrib.auth.models import Group
 from .decorators import *
 from .filter import *
 
+
+@login_required(login_url='login')
+@admin_only
+def home(requet):
+    breakfast = Category.objects.filter(name='breakfast')[0].fooditem_set.all()[:5]
+    lunch = Category.objects.filter(name='lunch')[0].fooditem_set.all()[:5]
+    dinner = Category.objects.filter(name='dinner')[0].fooditem_set.all()[:5]
+    snack = Category.objects.filter(name='snack')[0].fooditem_set.all()[:5]
+    customers = Customer.objects.all()
+    context = {
+        'breakfast': breakfast,
+        'lunch': lunch,
+        'dinner': dinner,
+        'snack': snack,
+        'customers': customers
+    }
+    return render(requet, 'main.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def home(requet):
+    breakfast = Category.objects.filter(name='breakfast')[0].fooditem_set.all()[:5]
+    lunch = Category.objects.filter(name='lunch')[0].fooditem_set.all()[:5]
+    dinner = Category.objects.filter(name='dinner')[0].fooditem_set.all()[:5]
+    snack = Category.objects.filter(name='snack')[0].fooditem_set.all()[:5]
+    customers = Customer.objects.all()
+    context = {
+        'breakfast': breakfast,
+        'lunch': lunch,
+        'dinner': dinner,
+        'snack': snack,
+        'customers': customers
+    }
+    return render(requet, 'main.html', context)
